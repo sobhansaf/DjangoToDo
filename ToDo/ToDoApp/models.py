ad33843e315372378cedf9ec02737c5ad7e6c142
sobhan <sobhan.saf79@gmail.com>
2021-10-07 09:24:06 +0330

    todo model is added

--- ToDo/ToDoApp/models.py
+++ ToDo/ToDoApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    date = models.DateField(default=timezone.now)
