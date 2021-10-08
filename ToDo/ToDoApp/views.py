7445af7cea2a1c9b396f8a3e8592edb66e4ac212
sobhan <sobhan.saf79@gmail.com>
2021-10-08 10:29:20 +0330

    using redirect to respond post request
    instead of responding to a wrong post request directly, we respond a redirect and save data in session to reproduce errors after redirection. also there were some changes in signup template

--- ToDo/ToDoApp/views.py
+++ ToDo/ToDoApp/views.py
@@ -10,15 +10,27 @@ class index(View):
 
 class SignUp(View):
     def post(self, request):
+        # response to a post request is redirect
         form = SignupForm(request.POST)
         if form.is_valid():
-            print('valid')
             form.save()
             user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
             login(request, user)
             return redirect('index')
-        return render(request, 'signup.html', {'signupform': form})
+        
+        # saving data in session, in order to show errors after redirection
+        request.session['wrong'] = True
+        request.session['data'] = request.POST
+        return redirect('signup')        
 
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('index')
         form = SignupForm()
+        if request.session.get('wrong', False):
+            # if the request was the result of a redirect after a post request with wrong data
+            form = SignupForm(request.session.get('data'))
+            request.session.pop('data')
+            request.session.pop('wrong')
+            form.is_valid()
         return render(request, 'signup.html', {'signupform': form})
