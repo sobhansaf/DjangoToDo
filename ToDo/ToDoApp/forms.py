0bb42853ef9e75a5ccd7d28f2ef4f9aefe73c06a
sobhan <sobhan.saf79@gmail.com>
2021-10-11 10:58:32 +0330

    added form for todo

---
+++ ToDo/ToDoApp/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import Todo
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
+class ToDoForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Todo
+        exclude = ('user', )
+        widgets = {
+            'date': DateInput(),
+            'time': TimeInput()
+        }
+
